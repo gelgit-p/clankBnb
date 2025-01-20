8712ee8e157429e02a52ddc2dd24ef9b014d97be
Sparkidea25 <michaeldaralola123@gmail.com>
2025-01-20 15:43:37 +0100

    listings

--- src/routes/listings.lazy.tsx
+++ src/routes/listings.lazy.tsx
@@ -113,7 +113,7 @@ function ListingsPage() {
   
 
 
-  const {data: getlistings, error, isPending} = useReadContract({
+  const {data: getlistings} = useReadContract({
     ...wagmiContractConfig,
     // address: '0x6b175474e89094c44da98b954eedeac495271d0f',
     functionName: 'getAllListing',
